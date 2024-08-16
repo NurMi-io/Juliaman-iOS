71c3225eb0688675f43a645b9fe64af2602a1dd6
NurMi App <sasha.rostovskaya@bk.ru>
2024-08-16 16:11:39 +0300

    Create JuliamanBridgingHeader.h

---
+++ TEXT/JuliamanBridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  SwiftParseChat-Bridging-Header.h
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 2/20/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+#import <ProgressHUD/ProgressHUD.h>
